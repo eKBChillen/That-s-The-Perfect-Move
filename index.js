ca84565de62b4173d01c9374511a782f3e4b1f25
eKBChillen <bfergy06@gmail.com>
2026-04-14 14:19:11 -0500

    email form
    updated the email form

---
+++ index.js
@@ -0,0 +1,18 @@
+function sendMail() {
+    const templateParams = {
+        name: document.querySelector("#name").value,
+        number: document.querySelector("#number").value,
+        email: document.querySelector("#email").value,
+        message: document.querySelector("#message").value,
+    }
+    emailjs
+    .send("service_sfpn8s9", "template_8w09g98", templateParams)
+    .then(() => {
+        alert("Message sent successfully");
+    })
+    .catch((error) => {
+        console.log("Error sending message:", error);
+        alert("There was an error sending your message. Please try again later.");
+    });
+
+}   
