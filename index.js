c126f617ab876b3334ca10e4e34d20e41bd94dbf
eKBChillen <bfergy06@gmail.com>
2026-04-15 10:36:17 -0500

    email update
    email update

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
 function sendMail() {
-    const templateParams = {
-        name: document.querySelector("#name").value,
-        number: document.querySelector("#number").value,
-        email: document.querySelector("#email").value,
-        message: document.querySelector("#message").value,
+    let parms = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value,
+        "phone number": document.getElementById("phone").value,
     }
-
-    Form.addEventListener("submit", function(event) {
-        event.preventDefault();
-    });
-    
-    emailjs.send("service_9l7vt7k", "template_8w09g98", templateParams)
+    emailjs.send("service_9l7vt7k", "template_8w09g98", parms)
     .then(() => {
         alert("Message sent successfully");
     })
     .catch((error) => {
         console.log("Error sending message:", error);
         alert("There was an error sending your message. Please try again later.");
-    });
-
-}   
+        });
+    }
